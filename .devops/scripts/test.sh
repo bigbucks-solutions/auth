ab694ccd5e084139ec885eac02bf90b0b72512f2
jamshi-dev <jamsheed@nsmail.dev>
2024-12-16 22:16:35 +0400

    chore: ci update

--- .devops/scripts/test.sh
+++ .devops/scripts/test.sh
@@ -1,4 +1,4 @@
 export PATH="$PATH:$(go env GOPATH)/bin"
 go mod tidy
 # ginkgo -r
-go run github.com/onsi/ginkgo/v2/ginkgo -r --fail-on-pending --fail-on-empty --keep-going --cover --coverprofile=cover.profile --race --trace ---github-output --poll-progress-after=10s --poll-progress-interval=10s .
+go run github.com/onsi/ginkgo/v2/ginkgo -r --fail-on-pending --fail-on-empty --keep-going --cover --coverprofile=cover.profile --race --trace --github-output --poll-progress-after=10s --poll-progress-interval=10s .
